5791b7a97c48a081bd82f0af057a1d447e5df1b1
sydneywylie <gamingmonkey2287@gmail.com>
2023-02-16 15:11:04 +0000

    sensor + graphing

--- sensor.py
+++ sensor.py
@@ -1,14 +1,28 @@
 import serial
 import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 ser = serial.Serial('COM3', 9600)
 time.sleep(2)
 
-while True:
+temperatureList = []
+timeList = []
+timeRange = int(input("How many seconds would you like to observe for? "))
+
+for i in range(timeRange):
     line = ser.readline()  
     if line:
         string = line.decode()  
-        temperature = float(string[-7:])
-        print(temperature)
-    if temperature > 21.00:
-        print("Room is above comfortable levels, heating will be reduced")      
+        temperature = float(string[:5])
+        touch = float(string[5:])
+        temperatureList.append(temperature)
+        timeList.append(i+1)
+    #if temperature > 21.00:
+        #print("Room is above comfortable levels, heating will be reduced")      
+
+xpoints = timeList
+ypoints = temperatureList
+
+plt.plot(xpoints, ypoints)
+plt.show()
