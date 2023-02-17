e8e42833241d21ad422b1a4b083b3f8bdee6a4bc
sydneywylie <gamingmonkey2287@gmail.com>
2023-02-17 12:02:08 +0000

    light finish and temp finish done

---
+++ lightfinish.py
@@ -0,0 +1,48 @@
+import serial
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import speech_recognition as sr
+import re
+
+ser = serial.Serial('COM3', 9600)
+time.sleep(2)
+
+def lightTurn(var):
+    while True:      #Do this in loop
+        if (var == '1'): #if the value is 1
+            ser.write(b'1') #send 1
+            print ("Light turned ON")
+            time.sleep(1)
+        
+        if (var == '0'): #if the value is 0
+            ser.write(b'0') #send 0
+            print ("Light turned OFF")
+            time.sleep(1)
+
+
+
+r=sr.Recognizer()
+mic=sr.Microphone()
+speech= sr.AudioFile('harvard.wav')
+def interpret(r,mic):
+    with mic as source:
+        r.adjust_for_ambient_noise(source,duration=0.5)
+        print("ready")
+        audio = r.listen(source)
+    #====
+    return r.recognize_sphinx(audio)
+    #return r.recognize_google(audio)
+    #==change to google for demonstration==
+
+#output=interpret(r,mic)
+output="turn lights off"
+print(output)
+
+if output=="turn lights on":
+    #====
+    var = '1'
+    lightTurn(var)
+elif output=="turn lights off":
+    var = '0'
+    lightTurn(var)
--- tempfinish.py
+++ tempfinish.py
@@ -2,6 +2,8 @@ import serial
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+import speech_recognition as sr
+import re
 
 ser = serial.Serial('COM3', 9600)
 time.sleep(2)
@@ -9,7 +11,8 @@ time.sleep(2)
 def tempratureFinder():
     temperatureList = []
     timeList = []
-    timeRange = int(input("How many seconds would you like to observe for? "))
+    #timeRange = int(input("How many seconds would you like to observe for? "))
+    timeRange = 10
 
     for i in range(timeRange):
         line = ser.readline()  
@@ -20,6 +23,39 @@ def tempratureFinder():
             temperatureList.append(temperature)
             timeList.append(i+1)
             print(temperature)
+    
+
+r=sr.Recognizer()
+mic=sr.Microphone()
+speech= sr.AudioFile('harvard.wav')
+def interpret(r,mic):
+    with mic as source:
+        r.adjust_for_ambient_noise(source,duration=0.5)
+        print("ready")
+        audio = r.listen(source)
+    #====
+    return r.recognize_sphinx(audio)
+    #return r.recognize_google(audio)
+    #==change to google for demonstration==
+
+#output=interpret(r,mic)
+output="what is the temperature"
+print(output)
+
+tempresult=re.search("what is the temperature",output)
+if tempresult.group() == "what is the temperature":
+    #====
+    tempratureFinder()
+
+    #==replace with actual temperature==
+    
+    #    temperature =sensor.temperature
+    #    print (temperature)
+
+
+
+
+
 
 
 
