7683c3d54adb118bcda2fef384a0b9f7a6a21e1c
sydneywylie <gamingmonkey2287@gmail.com>
2023-02-17 11:21:08 +0000

    function made

--- sensor.py
+++ sensor.py
@@ -6,23 +6,24 @@ import numpy as np
 ser = serial.Serial('COM3', 9600)
 time.sleep(2)
 
-temperatureList = []
-timeList = []
-timeRange = int(input("How many seconds would you like to observe for? "))
+def tempratureFinder():
+    temperatureList = []
+    timeList = []
+    timeRange = int(input("How many seconds would you like to observe for? "))
 
-for i in range(timeRange):
-    line = ser.readline()  
-    if line:
-        string = line.decode()  
-        temperature = float(string[:5])
-        touch = float(string[5:])
-        temperatureList.append(temperature)
-        timeList.append(i+1)
-    #if temperature > 21.00:
-        #print("Room is above comfortable levels, heating will be reduced")      
+    for i in range(timeRange):
+        line = ser.readline()  
+        if line:
+            string = line.decode()  
+            temperature = float(string[:5])
+            touch = float(string[5:])
+            temperatureList.append(temperature)
+            timeList.append(i+1)
+        #if temperature > 21.00:
+            #print("Room is above comfortable levels, heating will be reduced")      
 
-xpoints = timeList
-ypoints = temperatureList
+    xpoints = timeList
+    ypoints = temperatureList
 
-plt.plot(xpoints, ypoints)
-plt.show()
+    plt.plot(xpoints, ypoints)
+    plt.show()
